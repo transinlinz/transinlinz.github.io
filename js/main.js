a52a2c9da92984c56e0398d7cfdba4f51beece8b
Lena <transinlinz@protonmail.com>
2023-01-31 23:54:55 +0100

    Add site notice and improve styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 window.setTimeout(() => {
-  const codes = [116, 114, 97, 110, 115, 105, 110, 108, 105, 110, 122, 55356, 112, 114, 111, 116, 111, 110, 109, 97, 105, 108, 46, 99, 111, 109];
-  const name = codes.map(n => String.fromCharCode(n)).join('');
+  const codes = [
+    116, 114, 97, 110, 115, 105, 110, 108, 105, 110, 122, 0x2709, 112, 114, 111,
+    116, 111, 110, 109, 97, 105, 108, 46, 99, 111, 109,
+  ];
+  const name = codes.map((n) => String.fromCharCode(n)).join('');
 
   Array.prototype.forEach.call(document.querySelectorAll('a.ml'), (anchor) => {
     anchor.textContent = name;
@@ -8,8 +11,12 @@ window.setTimeout(() => {
 }, 0);
 
 window.setTimeout(() => {
-  const codes = [109, 97, 105, 108, 116, 111, 58, 116, 114, 97, 110, 115, 105, 110, 108, 105, 110, 122, 64, 112, 114, 111, 116, 111, 110, 109, 97, 105, 108, 46, 99, 111, 109];
-  const str = codes.map(n => String.fromCharCode(n)).join('');
+  const codes = [
+    109, 97, 105, 108, 116, 111, 58, 116, 114, 97, 110, 115, 105, 110, 108, 105,
+    110, 122, 64, 112, 114, 111, 116, 111, 110, 109, 97, 105, 108, 46, 99, 111,
+    109,
+  ];
+  const str = codes.map((n) => String.fromCharCode(n)).join('');
 
   Array.prototype.forEach.call(document.querySelectorAll('a.ml'), (anchor) => {
     anchor.addEventListener('click', (e) => {
@@ -17,4 +24,4 @@ window.setTimeout(() => {
       window.open(str);
     });
   });
-}, 1500);
+}, 500);
